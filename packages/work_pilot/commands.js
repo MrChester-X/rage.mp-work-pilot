93d0214fcc0a6f0fa088bc6a50dee53667216de0
MrChester-X <bulychev_platon1@mail.ru>
2022-05-19 22:12:36 +0300

    Add first plane position

--- packages/work_pilot/commands.js
+++ packages/work_pilot/commands.js
@@ -1,10 +1,14 @@
-count = 1
+spawnPlanes = [
+    {x: -1439.4000244140625, y: -2663.916259765625, z: 22.7186222076416, heading: -85.31299591064453}
+]
 
-mp.events.add("pos", (player) => {
+var countPos = 1
+
+mp.events.addCommand("pos", (player) => {
     let pos = player.position;
     let heading = player.heading;
 
-    console.log(`${count}. Position: ${pos} | Heading: ${heading}`);
+    console.log(`${count}. {x: ${pos.x}, y: ${pos.y}, z: ${pos.z}, heading: ${heading}}`);
 
     count++;
 });
